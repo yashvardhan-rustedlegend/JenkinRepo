d60de838942b160520c782bac81b46ed4652fda1
Yash Vardhan Tyagi <mananpushkar@gmail.com>
2023-02-17 00:19:01 +0530

    a update

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld{
 	public static void main(String args[])
 	{
-		System.out.println("Hello Yash Welcome to Jenkins");
+		int a=10;
+		System.out.println("Hello Yash Welcome to Jenkins" +a);
 	}
 
 }
